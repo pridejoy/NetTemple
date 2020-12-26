fcadd7c16e688828541024982978d09cd8cb6652
Pridejoy <pridejoy@foxmail.com>
2020-12-26 17:41:08 +0800

    oop基础

---
+++ File/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            FileStream fs= new FileStream("a.txt",FileMode.CreateNew);
+            StreamWriter sw =new StreamWriter(fs);
+            sw.WriteLine("张三");
+            sw.WriteLine("换行");
+            sw.Close();
+            fs.Close();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
