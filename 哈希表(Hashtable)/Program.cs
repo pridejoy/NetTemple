2636433b42ff4bf6849c4e00754b2d394b730efc
Pridejoy <pridejoy@foxmail.com>
2021-01-19 17:36:07 +0800

    泛型，文件，有序列表

--- 哈希表(Hashtable)/Program.cs
+++ 哈希表(Hashtable)/Program.cs
@@ -31,6 +31,29 @@ namespace 哈希表_Hashtable_
 
             //【实例】使用 Hashtable 集合实现图书信息的添加、查找以及遍历的操作。
 
+            Hashtable ht = new Hashtable();
+            ht.Add(1, "计算机基础");
+            ht.Add(2, "C#高级编程");
+            ht.Add(3, "数据库应用");
+            Console.WriteLine("请输入图书编号");
+            int id = int.Parse(Console.ReadLine());
+            bool flag = ht.ContainsKey(id);
+            if (flag)
+            {
+                Console.WriteLine("您查找的图书名称为：{0}", ht[id].ToString());
+            }
+            else
+            {
+                Console.WriteLine("您查找的图书编号不存在！");
+            }
+
+            Console.WriteLine("所有的图书信息如下：");
+            foreach (DictionaryEntry d in ht)
+            {
+                int key = (int)d.Key;
+                string value = d.Value.ToString();
+                Console.WriteLine("图书编号：{0}，图书名称：{1}", key, value);
+            }
 
         }
     }
---
+++ 有序列表(SortedList)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace 有序列表_SortedList_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //C# SortedList 类实现了 IDictionary 接口 ,集合中的值都是以键值对的形式存取的。
+
+            //C# SortedList 称为有序列表，按照 key 值对集合中的元素排序。
+
+            //SortedList 集合中所使用的属性和方法与 Hashtable  比较类似，这里不再赘述。
+
+            //下面通过实例来演示 SortedList 集合的使用。
+
+            // 【实例】使用 SortedList 实现挂号信息的添加、查找以及遍历操作。
+
+            SortedList sortList = new SortedList();
+            sortList.Add(1, "小张");
+            sortList.Add(2, "小李");
+            sortList.Add(3, "小刘");
+            Console.WriteLine("请输入挂号编号：");
+            int id = int.Parse(Console.ReadLine());
+            bool flag = sortList.ContainsKey(id);
+            if (flag)
+            {
+                string name = sortList[id].ToString();
+                Console.WriteLine("您查找的患者姓名为：{0}", name);
+            }
+            else
+            {
+                Console.WriteLine("您查找的挂号编号不存在！");
+            }
+            Console.WriteLine("所有的挂号信息如下：");
+            foreach (DictionaryEntry d in sortList)
+            {
+                int key = (int)d.Key;
+                string value = d.Value.ToString();
+                Console.WriteLine("挂号编号：{0}，姓名：{1}", key, value);
+            }
+
+        }
+    }
+}
