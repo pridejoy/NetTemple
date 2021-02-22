eb49e0cebb6521d279ae4300e9421bf0bc662e65
pride <pridejoy@foxmail.com>
2021-02-22 21:31:49 +0800

    2021-02-22 webapi

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //内部配置 会配置kesterl，iss 相关，其他配置
+           IHostBuilder hostBuilder= CreateHostBuilder(args);
+            //CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
